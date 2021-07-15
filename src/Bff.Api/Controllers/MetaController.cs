02b8010e8f5516fd9ba02bc981f66733992e6685
Allan Hardy <al_hardy@live.com.au>
2021-07-15 21:02:14 +1000

    adding ping endpoint

---
+++ src/Bff.Api/Controllers/MetaController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bff.Api.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [ApiController]
+    public class MetaController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public ActionResult<string> Ping()
+        {
+            return Ok("pong");
+        }
+    }
+}
