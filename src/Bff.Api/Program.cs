9e9a6da5fcbaeff3b22ad57e771fab0c2980c9b0
Allan Hardy <al_hardy@live.com.au>
2021-08-27 20:14:27 +1000

    adding serilog with loki formatter

--- src/Bff.Api/Program.cs
+++ src/Bff.Api/Program.cs
@@ -1,21 +1,50 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Grafana.Loki;
 
 namespace Bff.Api
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(new LokiJsonTextFormatter())
+                .CreateBootstrapLogger();
+            
+            Log.Information("Starting up");
+            
+            try
+            {
+                var host = CreateHostBuilder(args).Build();
 
-            await host.RunAsync();
+                await host.RunAsync();
+
+                Log.Information("Stopped");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console())
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
         }
     }
